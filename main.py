b9d91423ad9fba0eeb91b6ba8eafbce34fd4d2df
Ibtehal.Moahmed <Ibtehal.Moahmed@syngenta.com>
2023-03-16 11:50:14 -0400

    Day 26: Add the table

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ filepaths = glob.glob('Excel_Files/*.xlsx')
 
 for file_path in filepaths:
     print(file_path)
-    df = pd.read_excel(file_path, sheet_name='Sheet 1')
-    print(df)
 
     pdf = FPDF(orientation='p',unit='mm',format='A4')
     pdf.add_page()
@@ -20,6 +18,29 @@ for file_path in filepaths:
 
     invoice_name = file_name.split('-')[1]
     pdf.cell(w=50, h=8, txt=f'Date {Date}', align='L', ln=1)
+
+
+
+    df = pd.read_excel(file_path, sheet_name='Sheet 1')
+    print(df)
+
+    columns = list(df.columns)
+    columns = [str(c).replace('_',' ').title() for c in columns]
+    pdf.set_font(family='Times',style='b', size=12)
+    pdf.cell(w=30, h=8, txt=columns[0], align='L', border=1)
+    pdf.cell(w=70, h=8, txt=columns[1], align='L', border=1)
+    pdf.cell(w=40, h=8, txt=columns[2], align='L', border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], align='L', border=1)
+    pdf.cell(w=20, h=8, txt=columns[4], align='L', border=1, ln=1)
+
+    for index,row in df.iterrows():
+        pdf.set_font(family='Times', size=10)
+        pdf.set_text_color(80,80,80)
+        pdf.cell(w=30, h=8, txt=str(row['product_id']), align='L',border=1)
+        pdf.cell(w=70, h=8, txt=str(row['product_name']), align='L',border=1)
+        pdf.cell(w=40, h=8, txt=str(row['amount_purchased']), align='L',border=1)
+        pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), align='L',border=1)
+        pdf.cell(w=20, h=8, txt=str(row['total_price']), align='L',border=1,ln=1)
 
     pdf.output(f'PDFS/{file_name}.pdf')
     # print(invoice_name)
